18edb47619f2bfc5c24b547889b43e7ac74782bf
Daspoet <cederk2306@gmail.com>
2021-03-05 18:55:47 +0100

    Add func getKeyState

--- syscalls.go
+++ syscalls.go
@@ -3,6 +3,7 @@ package gowinkey
 import "syscall"
 
 var user32 = syscall.MustLoadDLL("user32")
+var keystate = user32.MustFindProc("GetAsyncKeyState")
 var mapVirtualKey = user32.MustFindProc("MapVirtualKeyA")
 
 // virtualKeyToString returns the string representation of vk.
@@ -12,4 +13,15 @@ var mapVirtualKey = user32.MustFindProc("MapVirtualKeyA")
 func virtualKeyToString(vk VirtualKey) string {
 	char, _, _ := syscall.Syscall(mapVirtualKey.Addr(), 2, uintptr(vk), 2, 0)
 	return string(char)
+}
+
+// getKeyState determines whether the given key is up or down
+// at the time this function is called, and whether the key
+// was pressed after a previous call to getKeyState.
+//
+// See https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-getasynckeystate
+// for more details.
+func getKeyState(key int) int32 {
+	state, _, _ := syscall.Syscall(keystate.Addr(), 1, uintptr(key), 0, 0)
+	return int32(state)
 }
