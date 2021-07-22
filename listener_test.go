0110b6765ca47e3222e05f740a4aa78e55f8e043
Daspoet <cederk2306@gmail.com>
2021-07-22 13:12:02 +0200

    Revert "fix(listener): apply pressed keys to events before checking predicates"
    This reverts commit 597329104ca0d85ade9d9ea1ae254c25ae0d8ee3, because
    listener_test.go should not have been committed.

--- listener_test.go
+++ listener_test.go
@@ -2,21 +2,8 @@ package gowinkey
 
 import (
 	"fmt"
-	"testing"
 	"time"
 )
-
-func TestPredicates(t *testing.T) {
-	events, stopFn := Listen(Hotkey(VK_1, VK_0))
-
-	time.AfterFunc(time.Minute, func() {
-		stopFn()
-	})
-
-	for e := range events {
-		fmt.Println(e)
-	}
-}
 
 func Example() {
 	events, stopFn := Listen()
