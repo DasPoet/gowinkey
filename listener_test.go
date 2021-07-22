597329104ca0d85ade9d9ea1ae254c25ae0d8ee3
Daspoet <cederk2306@gmail.com>
2021-07-22 13:04:49 +0200

    fix(listener): apply pressed keys to events before checking predicates

--- listener_test.go
+++ listener_test.go
@@ -2,8 +2,21 @@ package gowinkey
 
 import (
 	"fmt"
+	"testing"
 	"time"
 )
+
+func TestPredicates(t *testing.T) {
+	events, stopFn := Listen(Hotkey(VK_1, VK_0))
+
+	time.AfterFunc(time.Minute, func() {
+		stopFn()
+	})
+
+	for e := range events {
+		fmt.Println(e)
+	}
+}
 
 func Example() {
 	events, stopFn := Listen()
